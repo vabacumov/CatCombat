3283a85620f6bc923b8865a99124e50fa7e4090c
Vlad Abacumov <vabacumov@gmail.com>
2014-08-17 15:32:09 -0700

    sets default values for cat's main attributes, xp, level

--- db/migrate/20140816184143_create_cats.rb
+++ db/migrate/20140816184143_create_cats.rb
@@ -3,9 +3,10 @@ class CreateCats < ActiveRecord::Migration
     create_table :cats do |cat|
       cat.string :nickname
       cat.string :image_src
-      cat.integer :strength, :agility, :intelligence, :cuteness, :hp
-      cat.integer :xp
-      cat.integer :level
+      cat.integer :strength, :agility, :intelligence, :cuteness, default: 3
+      cat.integer :hp, default: 10
+      cat.integer :xp, default: 0
+      cat.integer :level, default: 0
       cat.integer :rank
       cat.integer :user_id
 
