38b5fd006b4a8c550d4451dc7c7c2b80feff009b
Vlad Abacumov <vabacumov@gmail.com>
2014-08-16 20:47:13 -0700

    adds post route for sign up and adds sign up logic to user model

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,17 @@
 get '/' do
   erb :index
 end
+
+post '/user/new' do
+  @user = User.new(fullname: params[:fullname], email: params[:email])
+  @user.password = params[:password]
+  @user.save!
+
+  session[:id] = @user.id
+  session[:email] = @user.email
+  redirect '/'
+end
+
+post 'cat/new' do
+
+end
