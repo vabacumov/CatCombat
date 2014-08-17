3f217f92a5ff4bbe71d2dbcff9277a87c1f913c0
Vlad Abacumov <vabacumov@gmail.com>
2014-08-16 22:26:36 -0700

    adds new routes for signing up, signing in

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -9,9 +9,27 @@ post '/user/new' do
 
   session[:id] = @user.id
   session[:email] = @user.email
-  redirect '/'
+  content_type :json
+  sign_out_div = erb :sign_out, :layout => false
+  {html: sign_out_div}.to_json
 end
 
-post 'cat/new' do
+get '/sessions/new' do
+  @user = User.find_by_email(params[:email])
+  if @user.password == params[:password]
+    session[:id] = @user.id
+    session[:email] = @user.email
+    content_type :json
+    sign_out_div = erb :sign_out, :layout => false
+    {html: sign_out_div}.to_json
+  else
+    redirect '/'
+  end
+end
 
+get '/sessions/delete' do
+  session.delete(:id)
+  content_type :json
+  sign_in_div = erb :sign_in, :layout => false
+  {html: sign_in_div}.to_json
 end
