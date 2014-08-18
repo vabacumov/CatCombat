508c557b9cc7f6b08db7ca67942f85edc007e0e5
Vlad Abacumov <vabacumov@gmail.com>
2014-08-17 21:52:20 -0700

    adds and modifies routes for creating new cat, choosing attributes fixes some conditional logic for authentication

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -35,28 +35,90 @@ end
 
 get '/sessions/new' do
   @user = User.find_by_email(params[:email])
-  if @user.password == params[:password]
-    session[:id] = @user.id
-    session[:email] = @user.email
-    @error = false
-    content_type :json
-    success = true
-    sign_out_div = erb :sign_out, :layout => false
-    {html: sign_out_div, success: success}.to_json
+  if @user
+    if @user.password == params[:password]
+      session[:id] = @user.id
+      session[:email] = @user.email
+      session[:cat_id] = @user.cat.id
+      @error = false
+      content_type :json
+      success = true
+      sign_out_div = erb :sign_out, :layout => false
+      user_cat_div = erb :user_cat, layout: false
+      {sign_out: sign_out_div, user_cat: user_cat_div, success: success}.to_json
+    else
+      @error = true
+      content_type :json
+      success = false
+      sign_in_div = erb :sign_in, :layout => false
+      {html: sign_in_div, success: success}.to_json
+    end
   else
-    @error = true
-    content_type :json
-    success = false
-    sign_in_div = erb :sign_in, :layout => false
-    {html: sign_in_div, success: success}.to_json
+      @error = true
+      content_type :json
+      success = false
+      sign_in_div = erb :sign_in, :layout => false
+      {html: sign_in_div, success: success}.to_json
   end
 end
 
 get '/sessions/delete' do
   session.delete(:id)
+  session.delete(:cat_id)
   @error = false
   content_type :json
   sign_in_div = erb :sign_in, :layout => false
   create_account_div = erb :create_account, :layout => false
   {login: sign_in_div, account: create_account_div}.to_json
+end
+
+post '/cats/new' do
+  @cat = Cat.new(nickname: params[:nickname], image_src: params[:url], user_id: params[:user_id])
+  if @cat.valid?
+    @cat.save!
+    session[:cat_id] = @cat.id
+    content_type :json
+    valid = true
+    choose_attributes = erb :cat_attributes, :layout => false
+    {html: choose_attributes, success: valid}.to_json
+  else
+    content_type :json
+    nickname_error = @cat.errors.messages[:nickname]
+    valid = false
+    create_cat_div = erb :create_cat, :layout => false
+    {html: create_cat_div, success: valid, error: nickname_error}.to_json
+  end
+end
+
+post '/cats/new/attributes' do
+  @cat = Cat.find(session[:cat_id])
+  if (params[:strength].to_i + params[:agility].to_i + params[:intelligence].to_i + params[:cuteness].to_i) == 5
+    @cat.update(strength: @cat.strength+params[:strength].to_i, agility: @cat.agility+params[:agility].to_i, intelligence: @cat.intelligence+params[:intelligence].to_i, cuteness: @cat.cuteness+params[:cuteness].to_i)
+    content_type :json
+    valid = true
+    strength = params[:strength]
+    agility = params[:agility]
+    intelligence = params[:intelligence]
+    cuteness = params[:cuteness]
+    xp = @cat.xp
+    level = @cat.level
+    rank = @cat.rank || "-"
+    cat_div = erb :user_cat, layout: false
+    {
+      cat: cat_div,
+      strength: strength,
+      agility: agility,
+      intelligence: intelligence,
+      cuteness: cuteness,
+      xp: xp,
+      level: level,
+      rank: rank,
+      success: valid
+    }.to_json
+  else
+    content_type :json
+    error = "Retry"
+    choose_attributes = erb :cat_attributes, :layout => false
+    {user_cat: choose_attributes, error: error}.to_json
+  end
 end
